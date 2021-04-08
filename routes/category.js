3153e0b11ee48e8760740f35e37d7bfc902f6b26
ashulyak <alexey.shulyak@vrpconsulting.com>
2021-04-08 16:30:52 +0300

    create functional upload added mdlw

--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,12 @@
 const passport = require('passport');
 const router = require('express').Router();
 const controller = require('../controllers/category');
+const upload = require('../middelware/upload');
 
 router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll);
 router.get('/:id', passport.authenticate('jwt', {session: false}), controller.getById);
 router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.remove);
-router.post('/', passport.authenticate('jwt', {session: false}),  controller.create);
-router.patch('/:id', passport.authenticate('jwt', {session: false}),  controller.update);
+router.post('/', passport.authenticate('jwt', {session: false}), upload.single('image'),  controller.create);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), upload.single('image'),  controller.update);
 
 module.exports = router;
