9c7a96d7a02a27c8172da5c630e34059b2c412d8
ashulyak <alexey.shulyak@vrpconsulting.com>
2021-04-08 14:18:48 +0300

    added cors morgan

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const morgan = require("morgan");
 // const mongoose = require("mongoose");
 // const passport = require('passport');
 //
@@ -22,11 +20,11 @@ const positionRoutes = require("./routes/position");
 // app.use(passport.initialize());
 // require('./middelware/passport')(passport);
 //
-// app.use(morgan("dev"));
+app.use(require("morgan")("dev"));
 // app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(cors());
+app.use(require("cors")());
 
 // Routes
 app.use("/api/auth", authRoutes);
