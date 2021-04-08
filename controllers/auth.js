d3d1f1992a879e9dbfbcfa602b815188a3673acf
ashulyak <alexey.shulyak@vrpconsulting.com>
2021-04-08 15:15:21 +0300

    create registration added bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,8 @@
+const bcrypt = require('bcryptjs');
+
+const User = require("../models/User");
+const keys = require('../config/keys');
+
 module.exports.login = async function (req, res) {
     /**
      * @body email
@@ -9,4 +14,27 @@ module.exports.register = async function (req, res) {
      * @body email
      * @body password
      */
+
+    const candidate = await User.findOne({ email: req.body.email });
+
+    if (candidate) {
+        res.status(409).json({
+            message: "a user with this email has already been created",
+        });
+    } else {
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt),
+        });
+
+        try {
+            await user.save();
+            res.status(200).json(user);
+        } catch (e) {
+
+        }
+    }
 }
