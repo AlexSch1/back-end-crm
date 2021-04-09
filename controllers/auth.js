d238221c9ea02a5b52c7273c22146fc10e3ea38b
ashulyak <alexey.shulyak@vrpconsulting.com>
2021-04-09 22:20:43 +0300

    fix scripts

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,6 +26,7 @@ module.exports.login = async function (req, res) {
             }, keys.jwt, { expiresIn: 60*60 });
 
             res.status(200).json({
+                email: candidate.email,
                 token: `Bearer ${ token }`,
             })
         } else {
